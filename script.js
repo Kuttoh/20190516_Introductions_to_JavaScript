3167313227e87a4130b80ab7753ef06c1fc7c9a2
Kuttoh <kuttohisaac@gmail.com>
2019-05-19 16:48:18 +0300

    add interest calculator

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 //
 // My Sample App
 //
-/*let principle = 1000000,
-    tenor = 1
-    rate = 0.18
-
-interest = principle * tenor * rate
-
-console.log(interest)*/
 
 
-if (tenor === 12)  {
-    rate = 0.18;
+
+function interestCalculator(){
+    var principal = Number(document.getElementById('principal').value)
+    var tenor = Number(document.getElementById('tenor').value)
+
+    if (tenor === 12){
+        rate = 0.18
+    }
+    else if (tenor === 9){
+        rate = 0.15
+    }
+    else if(tenor === 6){
+        rate = 0.12
+    }
+    else if( tenor === 3){
+        rate = 0.09
+    }
+
+    document.getElementById('result').innerHTML= principal + (principal * (tenor/12) * rate);
 }
-else if (tenor === 9){
-    rate = 0.15;
-}
-else if (tenor === 6){
-    rate = 0.12;
-}
-else if (tenor <= 6){
-    rate = 0.09;
-}
-console.log('Rate is:', rate)
+document.getElementById('calculate').addEventListener('click', interestCalculator)
