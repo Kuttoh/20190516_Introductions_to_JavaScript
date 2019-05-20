78afd58564d59faa823e8a67a4bd856e3a3036e9
Kuttoh <kuttohisaac@gmail.com>
2019-05-20 05:52:02 +0300

    Add rates for other tenors

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function interestCalculator(){
     var principal = Number(document.getElementById('principal').value)
     var tenor = Number(document.getElementById('tenor').value)
 
-    if (tenor === 12){
+    if (tenor > 9){
         rate = 0.18
     }
-    else if (tenor === 9){
+    else if (tenor <= 9 && tenor > 6 ){
         rate = 0.15
     }
-    else if(tenor === 6){
+    else if(tenor <= 6 && tenor > 3){
         rate = 0.12
     }
-    else if( tenor === 3){
+    else if( tenor === 3 && tenor >= 1){
         rate = 0.09
     }
 
